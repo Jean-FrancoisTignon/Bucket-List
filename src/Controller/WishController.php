d88408def7578a004f107f7098ce0644e31ecfc2
Jean-François Tignon <jeanfrancois.tignon2023@campus-eni.fr>
2024-01-24 11:52:31 +0100

    TP - évolution module 5 (Accès à la base de données)

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\WishRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,14 +10,26 @@ use Symfony\Component\Routing\Annotation\Route;
 class WishController extends AbstractController
 {
     #[Route('/wishes', name: 'app_wish_list')]
-    public function list(): Response
+    public function list(WishRepository $wishRepository): Response
     {
-        return $this->render('wish/list.html.twig');
+        $wishes = $wishRepository->findBy(["isPublished" => true],["dateCreated" => "DESC"], 20);
+
+        return $this->render('wish/list.html.twig',[
+            "wishes" => $wishes
+        ]);
     }
 
     #[Route('/wishes/detail/{id}', name: 'app_wish_detail')]
-    public function detail($id): Response
+    public function detail($id, WishRepository $wishRepository): Response
     {
-        return $this->render('wish/detail.html.twig');
+        $wish = $wishRepository->find($id);
+        // s'il n'existe pas en bdd, on déclenche une erreur 404
+        if (!$wish){
+            throw $this->createNotFoundException('This wish do not exists! Sorry!');
+        }
+
+        return $this->render('wish/detail.html.twig', [
+            "wish" => $wish
+        ]);
     }
 }
